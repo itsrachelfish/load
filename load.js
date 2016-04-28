405aebe50f0a4cc1db628b7c5204257b50aa6033
Rachel Fish <itsrachelfish@gmail.com>
2016-04-28 04:20:57 -0600

    Added AMD / CommonJS support

--- load.js
+++ load.js
@@ -1,3 +1,22 @@
+// Check how the plugin should be exported
+(function(factory)
+{
+    if(typeof module === 'object' && module.exports)
+    {
+        // We're in Node or a CommonJS compatable environment
+        factory(require('wetfish-basic'));
+    }
+    else if(typeof define === 'function' && define.amd)
+    {
+        // We're in a browser being loaded with AMD (Require.js)
+        define(['wetfish-basic'], factory);
+    }
+    else
+    {
+        // We're in a browser, so assume everything has been loaded as global variables
+        factory(basic);
+    }
+}
 (function($)
 {
     var load = function(element, url)
@@ -14,9 +33,9 @@
     // Wetfish basic wrapper
     $.prototype.load = function(url)
     {
-        this.forEach(this.elements, function(index, element)
+        this.forEach(this.elements, function(element)
         {
             load(element, url);
         });
     }
-}(basic));
+}));
